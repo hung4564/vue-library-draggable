30f2277a0469e1ceced324b00f73f5d2e0efa386
hung.pv <hphamviet83@gmail.com>
2022-12-11 17:00:53 +0700

    update - type

---
+++ types/index.d.ts
@@ -0,0 +1,13 @@
+import { VueConstructor } from "vue";
+declare module "@hungpv4564/vue-libary-draggable" {
+  const DraggableContianer: VueConstructor;
+  const DraggablePopup: VueConstructor;
+  const DraggableSidebar: VueConstructor;
+  const DraggableBottom: VueConstructor;
+  export {
+    DraggableContianer,
+    DraggablePopup,
+    DraggableSidebar,
+    DraggableBottom
+  };
+}
